e3aa8bdb1e3ff281a7b3d84b2c343443ff9ec9cb
yeti <yetiabe9@gmail.com>
2026-06-23 22:13:01 +0300

    Intitial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const menuToggle = document.getElementById("menu-toggle");
+const navbar = document.getElementById("navbar");
+
+menuToggle.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+});
+
+const images = [
+    "./image/map.jpg",
+    "./image/traning.jpg",
+    "./image/5man.png"
+];
+
+let currentIndex = 0;
+const image = document.getElementById("myImage");
+
+setInterval(() => {
+    image.style.opacity = "0";
+
+    setTimeout(() => {
+        currentIndex = (currentIndex + 1) % images.length;
+        image.src = images[currentIndex];
+        image.style.opacity = "1";
+    }, 500);
+
+}, 3000);
+// DARK MODE
+const toggle = document.getElementById("themeToggle");
+
+toggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    toggle.textContent = document.body.classList.contains("dark") ? "☀️" : "🌙";
+});
